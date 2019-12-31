2361b1f450b02d43d3860ecbb0a2f0f3cff04c58
rohitraja <rohitraja1901@gmail.com>
2019-12-31 20:00:18 +0530

    DB scripts update

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 This Api is integrated with nearby-ui and used to send response to the client based on the router request.
 
 ## Framework, Languages & tools
-Framework: Express
+Framework: Express , Sequelize
 Libraries: swagger-express-middleware
 Lint: Airbnb
-Unit Testing Framework: Mocha Assertion Library : Chai
+Database: MySql
+Unit Testing Framework: Mocha 
+Assertion Library : Chai
 
 ## Project setup
 npm install
@@ -18,3 +20,5 @@ npm run test
 
 ## Lints and fixes files
 npm run lint
+
+## To run the application ,please find the db_scripts folder for database details , configure mysql db in local and provide correct connection details in src/dao/NearbyDatabaseManager.js class.
---
+++ db_scripts/nearby_toilet.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: nearby
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `toilet`
+--
+
+DROP TABLE IF EXISTS `toilet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `toilet` (
+  `Amenities` varchar(255) DEFAULT NULL,
+  `Toilets` varchar(255) DEFAULT NULL,
+  `Id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `toilet`
+--
+
+LOCK TABLES `toilet` WRITE;
+/*!40000 ALTER TABLE `toilet` DISABLE KEYS */;
+INSERT INTO `toilet` VALUES ('Soap Dispenser','Public Toilets',1),('Washbasin','Coal Farm Toilets',2),('Security Lights','Private Toilets',3),('Faucet for Washing','Male',4),('Lock on doors','Female',5),('Security Windows','Unisex',6);
+/*!40000 ALTER TABLE `toilet` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-31 19:25:21
